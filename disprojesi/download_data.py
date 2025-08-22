76ea0d04fd05d6452e5a5c0cc9974fa8c80afc37
ihsanagir <agir.ihsanfurkan@gmail.com>
2025-08-22 15:46:38 +0300

    Update download_data.py

--- disprojesi/download_data.py
+++ disprojesi/download_data.py
@@ -1,6 +1,7 @@
 from roboflow import Roboflow
 
-rf = Roboflow(api_key="8NSTWSAx6tJvS8zFRwac")
+rf = Roboflow(api_key="-------------------")
 project = rf.workspace("meyve-eyp6i").project("wisdom-teeth-nbnzt-mnmd0")
 version = project.version(2)
+
 dataset = version.download("yolov8")
